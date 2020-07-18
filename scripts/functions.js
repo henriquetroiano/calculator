9cabc50854e95d20cc2f5eecb281619f22fcec25
henriquetroiano <henriquetroiano@hotmail.com>
2020-07-18 20:48:34 -0300

    first commit

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,38 +1,66 @@
 const inputValue = document.querySelectorAll('#data');
 const numberButtons = document.querySelectorAll('#numbers button');
 const operationButtons = document.querySelectorAll('#operations button');
-const actionButtons = document.querySelectorAll('#actions button');
+const resultButtons = document.querySelectorAll('#result button');
 
+inputValue[0].value = 0;
 
 
 
 numberButtons.forEach((btn) => {
-
-    btn.addEventListener('click', function(){
-        let valueButton = +btn.value;
-        inputValue[0].value += valueButton;
+    
+    btn.addEventListener('click', function(event){
+        
+        if(event.target.id == "del") {
+            const inputDel = inputValue[0].value;
+            inputValue[0].value =  inputDel.slice(0, inputDel.length - 1);
+            if (inputValue[0].value.length == "") {
+                inputValue[0].value = 0;
+            }
+            
+        } else if(event.target.id == "clear") {
+            inputValue[0].value = 0;
+            console.log(event.target.id)
+        } else {
+            inputValue[0].value == 0 ? inputValue[0].value = '' : inputValue[0].value;
+            let valueButton = +btn.value;
+            inputValue[0].value += valueButton;
+        } 
+        
+        
+        
     })
 })
 
 operationButtons.forEach((btn) => {
-
+    
     btn.addEventListener('click', function(){
         let valueButton = btn.value;
         inputValue[0].value += valueButton;
     })
 })
 
-actionButtons.forEach((btn) => {
 
+
+
+
+
+
+
+
+resultButtons.forEach((btn) => {
+    
     btn.addEventListener('click', function() {
-        if(btn.id="clear") {
-            inputValue[0].value = 0;
-        }
-        if(btn.id="del") {
-            inputValue[0].value.filter(inputValue[0].value.length);
-        }
+
+        console.log(inputValue[0].value.split('X'))
+
+      
+        //  inputValue[0].value =  inputValue[0].value.split(' X ')
+        
+        
     })
 })
+
 
 
 
